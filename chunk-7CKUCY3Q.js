467566e8d48c71d15ce2903a4f84cfdb444f7a89
Diego M. <diegomenta@gmail.com>
2025-07-19 02:07:20 -0300

    Auto-generated commit

---
+++ chunk-7CKUCY3Q.js
@@ -0,0 +1,1 @@
+var t={category:{title:"Categor\xEDa",lowercase:"categor\xEDa"},tag:{title:"Etiqueta",lowercase:"etiqueta"}};export{t as a};
--- chunk-NO2H6IJC.js
+++
@@ -1,1 +0,0 @@
-var t={category:"Categor\xEDa",tag:"Etiqueta"};export{t as a};
